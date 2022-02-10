ca6e53276350d760b6846c579d5e49c18021e37b
Evgenii Galimov <egalimov95@gmail.com>
2022-02-10 20:08:04 +0300

    app.tsx -> functional component

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,10 @@
-import React, { Component } from "react";
+import React from "react";
 
-class App extends Component {
-  render() {
-    return (
-      <div>
-        <h1>Игра vvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvигрушка игрища!!!!!</h1>
-      </div>
-    );
-  }
+function App() {
+  return (
+    <div>
+      <h1>Игра игрушка игрища!!!!!</h1>
+    </div>
+  );
 }
 export default App;
