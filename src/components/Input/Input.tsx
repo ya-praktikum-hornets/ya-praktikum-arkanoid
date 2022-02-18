ae6e508270438a62e587b2661f04555de7b28c06
Evgood <gorohovev@list.ru>
2022-02-18 15:03:52 +0300

    Add Input component

---
+++ src/components/Input/Input.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import { InputElement } from './style';
+
+const Input = (props) => {
+    return (
+        <InputElement {...props} />
+    );
+}
+
+export default Input;
---
+++ src/components/Input/style.ts
@@ -0,0 +1,17 @@
+import styled from 'styled-components';
+
+const InputElement = styled.input`
+    width: 100%;
+    height: 48px;
+
+    background: #F4F4F5;
+    border: 1px solid #E8E8E8;
+    border-radius: 6px;
+    padding: 0 16px;
+
+    font-weight: 400;
+    font-size: 14px;
+    color: #272C32;
+`;
+
+export { InputElement };
