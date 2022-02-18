25c901f4bce20e4b37d311fbf321f972a103f777
Evgood <gorohovev@list.ru>
2022-02-18 15:09:50 +0300

    Update default export components index file

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,2 +1,8 @@
 export { default as Header } from './Header/Header';
 export { default as Nav } from './Nav/Nav';
+export { default as Button } from './Button/Button';
+export { default as Input } from './Input/Input';
+export { default as Form } from './Forms/Form/Form';
+export { default as LoginForm } from './Forms/LoginForm/LoginForm';
+export { default as SignInForm } from './Forms/SignInForm/SignInForm';
+export { default as Container } from './Container/Container';
