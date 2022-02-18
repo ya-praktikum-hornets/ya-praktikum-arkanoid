a1b3366e032e4fff3a67b3a48afa1975563b20c8
Evgood <gorohovev@list.ru>
2022-02-18 15:01:07 +0300

    Add Button component

---
+++ src/components/Button/Button.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import { ButtonElement } from './style';
+
+const Button = (props) => {
+    return (
+        <ButtonElement {...props} />
+    );
+}
+
+export default Button;
---
+++ src/components/Button/style.ts
@@ -0,0 +1,17 @@
+import styled from 'styled-components';
+
+const ButtonElement = styled.button`
+    width: 100%;
+    height: 52px;
+
+    border: none;
+
+    font-family: inherit;
+    font-weight: 600;
+    font-size: 14px;
+    color: #fff;
+    background: #0B82FF;
+    border-radius: 6px;
+`;
+
+export { ButtonElement };
