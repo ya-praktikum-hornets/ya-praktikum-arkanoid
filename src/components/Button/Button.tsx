e9fe21b0eeb04fc8534e41808052dd4209ebb9e1
Evgood <gorohovev@list.ru>
2022-02-20 19:48:28 +0300

    Update Button and Container interfaces

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -4,17 +4,18 @@ import { StyledButton } from './styled';
 
 export interface ButtonProps {
     primary?: boolean,
-    autoFocus?: boolean;
-    disabled?: boolean;
-    form?: string;
-    formAction?: string;
-    formEncType?: string;
-    formMethod?: string;
-    formNoValidate?: boolean;
-    formTarget?: string;
-    name?: string;
-    type?: 'submit' | 'reset' | 'button';
-    value?: string | string[] | number;
+    autoFocus?: boolean,
+    disabled?: boolean,
+    form?: string,
+    formAction?: string,
+    formEncType?: string,
+    formMethod?: string,
+    formNoValidate?: boolean,
+    formTarget?: string,
+    name?: string,
+    type?: 'submit' | 'reset' | 'button',
+    value?: string | string[] | number,
+    children?: React.ReactNode,
 }
 
 const Button = (props: ButtonProps) => {
