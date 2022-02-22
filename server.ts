2d5dde88a2b3a7c1ac771d7f71ac4d1b84f3c9bb
Evgenii Galimov <egalimov95@gmail.com>
2022-02-22 03:11:55 +0300

    add dev env

--- server.ts
+++ server.ts
@@ -2,13 +2,14 @@ import express from 'express'
 import path from 'path';
 const app = express();
 const PORT = process.env.PORT || 3000;
+const ENV = process.env.NODE_ENV
 import dotenv from 'dotenv'
 dotenv.config()
 
-
-app.use('/', express.static(path.join(__dirname, 'client')))
+console.log(process.env.NODE_ENV)
+app.use('/', express.static(path.join(__dirname, `${ENV === 'production' ? 'client' : 'build/client'}`)))
 app.use('*', (_, res) => {
-  res.sendFile(path.join(__dirname, 'client/index.html'));
+  res.sendFile(path.join(__dirname, `${ENV === 'production' ? 'client/index.html' : 'build/client/index.html'}`));
 });
 
 app.listen(PORT, () => {
